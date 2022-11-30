9b9736c4fc7757220ef36f864648c0c04e880244
yukr905 <63770062+yukr905@users.noreply.github.com>
2022-11-30 15:31:52 +0200

    warrior

--- index.ts
+++ index.ts
@@ -1,17 +1,20 @@
-import { json } from "stream/consumers";
-import { Warrior } from "./Characters/warrior";
+import {Mage} from "./Characters/mage"
+import { Warrior } from "./Characters/warrior"
+import {Thief} from "./Characters/thief"
 
 let Jhon = new Warrior("Jhon")
-let Frid = new Warrior("Frid")
-console.log(Jhon.Health)
+let Frid = new Thief("Frid")
+let Jhon1 = new Mage("Jhon1")
 console.log(Jhon.print())
+console.log(Jhon1.print())
+console.log(Frid.print())
 Jhon.protected()
 console.log(Jhon.protection)
 setTimeout(()=>{console.log(Jhon.protection)},4000)
 
-Jhon.fight(Frid.Damage)
-Jhon.fight(Frid.Damage)
-Jhon.fight(Frid.Damage)
-Jhon.fight(Frid.Damage)
+Jhon.deffend(Jhon1.Damage)
+Jhon.deffend(Jhon1.Damage)
+Jhon1.deffend(Frid.Damage)
+console.log(Jhon1.Health)
 console.log(Jhon.DeathScrole)
 console.log(Jhon.Health)
