2e870bb655082a9de28feca2a513e02d47ff3f68
yukr905 <63770062+yukr905@users.noreply.github.com>
2022-12-01 15:50:07 +0200

    warrior

--- Characters/warrior.ts
+++ Characters/warrior.ts
@@ -1,21 +1,20 @@
-
-export class Warrior{
-    name:string 
-    private deathScrole:number =0 
-    private health:number = 200
-    private damage:number = 50
-    protection:boolean = false
+import {Characters} from "./Character"
+export class Warrior extends Characters{
     constructor(name:string){
+        super()
         this.name = name
+        this.health = 200
+        this.maxHealth =200
+        this.damage = 50
+        this.protection = false
+        this.deathScrole = 0
     }
     print(){
         return `Warrior: ${this.name}`
     }
     protected(){  // временная способность пока что ничего не делает только появляется
-        this.protection = true
-        setTimeout(()=>{
-            this.protection = false        
-        },1000)
+        super.protected()
+        return "Protection activated"
     }
     public get Health():number{
         return this.health
@@ -23,12 +22,8 @@ export class Warrior{
     public get Damage():number{
         return this.damage
     }
-    public deffend(n:number){  // ПРОСТО ТЕСТ РАБОТАЕТ ЛИ 
-        this.health -= n   
-        if(this.health<=0){
-            this.health = 200
-            this.deathScrole++
-        }
+    attack(enemy:any){  // ПРОСТО ТЕСТ РАБОТАЕТ ЛИ 
+        super.attack(enemy)
     }
     public get DeathScrole():string{
         return `You died: ${this.deathScrole}`
