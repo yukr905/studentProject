4987b2db334782f3895815460585dc24dc343ed5
yukr905 <63770062+yukr905@users.noreply.github.com>
2022-12-04 14:04:26 +0200

    main

--- index.ts
+++ index.ts
@@ -1,9 +1,4 @@
-import {Factory} from "./pattern/Factory"
 
-let mage = Factory.createCharacter("Mage","Jhon")
-let warrior = Factory.createCharacter("Warrior","Jhon")
-console.log(mage.print())
-console.log(warrior.print())
 
 
 
