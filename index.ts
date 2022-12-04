e1cab5c75e56e65a4a63ae8cb7b1f7fde7b9a00e
yukr905 <63770062+yukr905@users.noreply.github.com>
2022-12-04 13:31:44 +0200

    main

--- index.ts
+++ index.ts
@@ -1,20 +1,10 @@
-import {Mage} from "./Characters/mage"
-import { Warrior } from "./Characters/warrior"
-import {Thief} from "./Characters/thief"
+import {Factory} from "./pattern/Factory"
 
-let Jhon = new Warrior("Jhon")
-let Frid = new Thief("Frid")
-let Jhon1 = new Mage("Jhon1")
-console.log(Jhon.print())
-console.log(Jhon1.print())
-console.log(Frid.print())
-console.log(Jhon.protected())
-console.log(Jhon1.protected())
-console.log(Jhon.Damage)
-console.log(Jhon1.Health)
-Jhon.attack(Jhon1)
-Jhon.attack(Jhon1)
-console.log(Jhon1.DeathScrole)
-console.log(Jhon1.Health)
+let mage = Factory.createCharacter("Mage","Jhon")
+let warrior = Factory.createCharacter("Warrior","Jhon")
+console.log(mage.print())
+console.log(warrior.print())
+
+
 
 
