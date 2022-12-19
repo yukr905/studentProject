bce2a7fd3b2d703e562a272d8c875fe278113964
yukr905 <63770062+yukr905@users.noreply.github.com>
2022-12-19 19:33:29 +0200

    handlerError

---
+++ middleware/handlerError.ts
@@ -0,0 +1,9 @@
+import {ApiError} from "./apiError"
+import { NextFunction, Request,Response } from 'express'
+
+export function handlerError (err: Error,req:Request,res:Response,next: NextFunction) {
+    if (err instanceof ApiError) {
+        return res.status(err.status).json({message: err.message})
+    }
+    return res.status(500).json({message: "Непредвиденная ошибка!"})
+}
