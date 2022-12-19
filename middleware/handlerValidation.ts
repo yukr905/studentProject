96292cf42ad6821a7aeb66f8e4cd2ffbe64d94a0
yukr905 <63770062+yukr905@users.noreply.github.com>
2022-12-19 19:31:50 +0200

    handlerValidation

--- middleware/handlerValidation.ts
+++ middleware/handlerValidation.ts
@@ -1,16 +1,25 @@
-// import { validationResult } from "express-validator"
-// import { NextFunction } from 'express';
+import { validationResult } from "express-validator"
+import { NextFunction, Request,Response } from 'express';
+import {ApiError} from "./apiError"
 
-// export default (req,res,next: NextFunction)=>{
-//     const errors = validationResult(req)
-//     if(!errors.isEmpty()){
-//         return res.status(400).json(errors.array())
-//     }
-//     if(req.body.password1 !== req.body.password2){
-//         return res.status(400).json({message:"Passwords do not match, please check again "})
-//     }
-//     if(req.body.class !== "Warrior" &&  req.body.class !=="Mage" && req.body.class !=="Thief"){
-//         return res.status(400).json({message:"Mage or Warrior or Thief"})
-//     }
-//     return next()
-// }
+export function handlerregister (req:Request,res:Response,next: NextFunction){
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json(errors.array())
+    }
+    if(req.body.password1 !== req.body.password2){
+        return next(ApiError.badRequest400("Passwords do not match, please check again "))
+    }
+    if(req.body.class !== "Warrior" &&  req.body.class !=="Mage" && req.body.class !=="Thief"){
+        return next(ApiError.badRequest400("Mage or Warrior or Thief"))
+    }
+    return next()
+}
+
+export function handlerlogin (req:Request,res:Response,next: NextFunction){
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json(errors.array())
+    }
+    return next()
+}
