d124fdb6413181012791548b42fd5102fdf2d392
yukr905 <63770062+yukr905@users.noreply.github.com>
2022-12-19 19:29:19 +0200

    userController

--- controller/userController.ts
+++ controller/userController.ts
@@ -1,52 +1,20 @@
 import {registerUser , getMeInfo , updateUser , loginUser} from "../service/userService"
-import { validationResult } from "express-validator"
+import {} from "../middleware/apiError"
 async function register(req:any,res:any){
-    try {
-        const errors = validationResult(req)
-        if(!errors.isEmpty()){
-            return res.status(400).json(errors.array())
-        }  
         const user = await registerUser(req.body)
         return res.status(201).json(user)
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({message:"Something went wrong "})
-    }
 }
 async function login(req:any,res:any){
-    try {
-        const errors = validationResult(req)
-        if(!errors.isEmpty()){
-            return res.status(400).json(errors.array())
-        }
         const token = await loginUser(req.body)
         return res.json(token)
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({message:"Something went wrong "})
-    }
 }
 async function getMe(req:any,res:any){
-    try {
         const user = await getMeInfo(req.userId)
         return res.json(user)
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({message:"Something went wrong "})
-    }
 }
 async function updateMe(req:any,res:any){
-    try {
-        const errors = validationResult(req)
-        if(!errors.isEmpty()){
-            return res.status(400).json(errors.array())
-        }
         const newUser = await updateUser(req.body)
         return res.json(newUser)
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({message:"Something went wrong "})
-    }
 }
 
 export {register,login,updateMe,getMe}
