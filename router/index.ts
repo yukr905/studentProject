6b420ee8762299a290fc34cf0445d077f8b9b815
yukr905 <63770062+yukr905@users.noreply.github.com>
2022-12-19 19:32:15 +0200

    router

--- router/index.ts
+++ router/index.ts
@@ -2,14 +2,13 @@ import Router from"express"
 
 import {login,register,getMe,updateMe}  from "../controller/userController"
 import {getAllClass} from"../controller/сharacterController"
-import { validationResult } from "express-validator"
+import {handlerregister, handlerlogin} from "../middleware/handlerValidation"
 import {loginValidation,registerValidation} from"../middleware/validation"
 
 const router = Router()
 
-
-router.post("/register",registerValidation, register)
-router.post("/login",loginValidation, login)
+router.post("/register",registerValidation,handlerregister , register)
+router.post("/login",loginValidation,handlerlogin, login)
 router.get("/me/:id", getMe)
 router.patch("/me:id",registerValidation, updateMe)
 router.get("/class", getAllClass)
