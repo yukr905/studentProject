29c245206d68eb08397cdc302282e8fa12dc67b6
yukr905 <63770062+yukr905@users.noreply.github.com>
2022-12-19 11:04:39 +0200

    update later

--- middleware/handlerValidation.ts
+++ middleware/handlerValidation.ts
@@ -1,10 +1,16 @@
-import { validationResult } from "express-validator"
-import { NextFunction } from 'express';
+// import { validationResult } from "express-validator"
+// import { NextFunction } from 'express';
 
-export default (req,res,next: NextFunction)=>{
-    const errors = validationResult(req)
-    if(!errors.isEmpty()){
-        return res.status(400).json(errors.array())
-    }
-    return next()
-}
+// export default (req,res,next: NextFunction)=>{
+//     const errors = validationResult(req)
+//     if(!errors.isEmpty()){
+//         return res.status(400).json(errors.array())
+//     }
+//     if(req.body.password1 !== req.body.password2){
+//         return res.status(400).json({message:"Passwords do not match, please check again "})
+//     }
+//     if(req.body.class !== "Warrior" &&  req.body.class !=="Mage" && req.body.class !=="Thief"){
+//         return res.status(400).json({message:"Mage or Warrior or Thief"})
+//     }
+//     return next()
+// }
