7b1cdc95ef8e816919c8e278a51f3ed1f71123eb
yukr905 <63770062+yukr905@users.noreply.github.com>
2022-12-19 19:29:46 +0200

    main

--- index.ts
+++ index.ts
@@ -1,38 +1,21 @@
 import  {router} from"./router/index"
 import express from "express"
 import WebSocket  from "ws"
+import {controllerWS} from "./controller/eventsController"
+import {handlerError} from "./middleware/handlerError"
 
 
 const app= express()
 app.use(express.json())
 app.use("/api" , router)
 
+app.use(handlerError)
 
 app.listen(3000,()=>{
   console.log("Server started : 3000")
 });
 
 const ws = new WebSocket.Server({port:3001}) 
-
-ws.on("connection",(ws)=>{
-  console.log("new connection")
-  ws.on("message",(message:string)=>{
-    let data = (JSON.parse(message))
-    if(data.message =="attack"){
-
-      ws.send(`attack ${data.id}`)
-    }
-    if(data.message =="protected"){
-      ws.send(`protected ${data.id}`)
-    }
-    if(data.message =="relive"){
-      ws.send(`relive ${data.id}`)
-    }
-    if(data.message =="print"){
-      ws.send(`print ${data.id} ${data.message}`)
-    }
-  })
-})
-
+controllerWS(ws)
 
 
