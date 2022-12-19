a4682f5a3ecdea64b23f2b6a48163c80aedc274e
yukr905 <63770062+yukr905@users.noreply.github.com>
2022-12-19 19:31:03 +0200

    characterController

--- controller/сharacterController.ts
+++ controller/сharacterController.ts
@@ -1,12 +1,7 @@
 import {allClass} from "../service/characterService"
 async function getAllClass(req:any,res:any){
-    try {
         const clases = await allClass()       
         return res.json(clases)
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({message:"Something went wrong "})
-    }
 }
 
 export {getAllClass}
