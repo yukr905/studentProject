529405f1e9bea02d96f0db3d2e2cc2565f859d11
yukr905 <63770062+yukr905@users.noreply.github.com>
2022-12-21 23:57:48 +0200

    characterController

--- controller/сharacterController.ts
+++ controller/сharacterController.ts
@@ -1,7 +1,10 @@
 import {allClass} from "../service/characterService"
-async function getAllClass(req:any,res:any){
-        const clases = await allClass()       
-        return res.json(clases)
+
+export class characterController{
+        static async getAllClass(req:any,res:any){
+                const clases = await allClass()       
+                return res.json(clases)
+        }
 }
 
-export {getAllClass}
+
