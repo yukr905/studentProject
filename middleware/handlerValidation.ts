0ac4a0e5094fc3c0f9a9ccdb8e25963f91cb242f
yukr905 <63770062+yukr905@users.noreply.github.com>
2022-12-21 23:59:03 +0200

    handlerValidation

--- middleware/handlerValidation.ts
+++ middleware/handlerValidation.ts
@@ -2,24 +2,28 @@ import { validationResult } from "express-validator"
 import { NextFunction, Request,Response } from 'express';
 import {ApiError} from "./apiError"
 
-export function handlerregister (req:Request,res:Response,next: NextFunction){
-    const errors = validationResult(req)
-    if(!errors.isEmpty()){
-        return res.status(400).json(errors.array())
+
+
+export class handlerValidation{
+    static handlerregister (req:Request,res:Response,next: NextFunction){
+        const errors = validationResult(req)
+        if(!errors.isEmpty()){
+            return res.status(400).json(errors.array())
+        }
+        if(req.body.password1 !== req.body.password2){
+            return next(ApiError.badRequest400("Passwords do not match, please check again "))
+        }
+        if(req.body.class !== "Warrior" &&  req.body.class !=="Mage" && req.body.class !=="Thief"){
+            return next(ApiError.badRequest400("Mage or Warrior or Thief"))
+        }
+        return next()
     }
-    if(req.body.password1 !== req.body.password2){
-        return next(ApiError.badRequest400("Passwords do not match, please check again "))
+    static handlerlogin (req:Request,res:Response,next: NextFunction){
+        const errors = validationResult(req)
+        if(!errors.isEmpty()){
+            return res.status(400).json(errors.array())
+        }
+        return next()
     }
-    if(req.body.class !== "Warrior" &&  req.body.class !=="Mage" && req.body.class !=="Thief"){
-        return next(ApiError.badRequest400("Mage or Warrior or Thief"))
-    }
-    return next()
 }
 
-export function handlerlogin (req:Request,res:Response,next: NextFunction){
-    const errors = validationResult(req)
-    if(!errors.isEmpty()){
-        return res.status(400).json(errors.array())
-    }
-    return next()
-}
