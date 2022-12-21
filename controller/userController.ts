2add1babfe843e1c827e9d9a1d6076a20e0c6ece
yukr905 <63770062+yukr905@users.noreply.github.com>
2022-12-21 23:57:28 +0200

    userController

--- controller/userController.ts
+++ controller/userController.ts
@@ -1,20 +1,20 @@
 import {registerUser , getMeInfo , updateUser , loginUser} from "../service/userService"
 import {} from "../middleware/apiError"
-async function register(req:any,res:any){
-        const user = await registerUser(req.body)
-        return res.status(201).json(user)
+export class userController{
+        static async  register (req:any,res:any){
+                const user = await registerUser(req.body)
+                return res.status(201).json(user)
+        }
+        static async  login(req:any,res:any){
+                const token = await loginUser(req.body)
+                return res.json(token)
+        }
+        static async  getMe(req:any,res:any){
+                const user = await getMeInfo(req.userId)
+                return res.json(user)
+        }
+        static async  updateMe(req:any,res:any){
+                const newUser = await updateUser(req.body)
+                return res.json(newUser)
+        }
 }
-async function login(req:any,res:any){
-        const token = await loginUser(req.body)
-        return res.json(token)
-}
-async function getMe(req:any,res:any){
-        const user = await getMeInfo(req.userId)
-        return res.json(user)
-}
-async function updateMe(req:any,res:any){
-        const newUser = await updateUser(req.body)
-        return res.json(newUser)
-}
-
-export {register,login,updateMe,getMe}
