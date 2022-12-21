5a6a05bd302779b9a46d65980d1b40b4f2ce2955
yukr905 <63770062+yukr905@users.noreply.github.com>
2022-12-21 23:57:03 +0200

    eventsController

--- controller/eventsController.ts
+++ controller/eventsController.ts
@@ -4,59 +4,52 @@ import {attack,ability,messageUser,relive,connectionDB,disconnectionDB} from "..
 
 let client:any = [] // список активных юзеров
 let id = 0 
-function controllerWS(ws:any){
-    ws.on("connection",(ws:any)=>{
+
+export class eventController{
+    static async connect(ws:any){
         console.log("new connection")
         id++
         ws.unicid = id // присваиваем уникальноу ID
         client.push(ws)
-        ws.on("close",()=>{
-            console.log("disconnection")
+        console.log(client)
+    }
+    static async disconnect(ws:any){
+        console.log("disconnection")
             client.forEach(function (value:any,i:number) {
                 if(value.readyState !== 1) {
                     client.splice(i, 1)
                 }
             });
-        })
-        ws.on("message",async (message:string)=>{
-            let data = JSON.parse(message)
-            if(data.event =="attack"){
-                console.log("attack")
-                //await attack(data)
-                ws.send(`attack ${data.id}`)
-            }
-            else if(data.event =="ability"){
-                console.log("ability")
-                //await ability(data)
-                ws.send(`protected ${data.id}`)
-            }
-            else if(data.event =="relive"){
-                console.log("relive")
-                //await relive(data)
-                ws.send(`relive ${data.id}`)
-            }
-            else if(data.event =="print"){
-                console.log("print")
-                //await messageUser(data)
-                if(!data.id){
-                    client.forEach(function (value:any) {
-                        if(value.readyState == 1 ) {
-                            return value.send(data.message)
-                        }
-                    })
-                }else{
-                    client.forEach(function (value:any) {
-                        if(value.readyState == 1 && value.unicid == data.id) {
-                            value.send(data.message)
-                        }
-                    })
-                }
-            }else{
-                ws.send("Неизвестное событие")
+    }
+    static async attack(ws:any , data:any){
+        console.log("attack")
+        //await attack(data)
+        ws.send(`attack ${data.id}`)
+    }
+    static async ability(ws:any , data:any){
+        console.log("ability")
+        //await ability(data)
+        ws.send(`protected ${data.id}`)
+    }
+    static async relive(ws:any , data:any){
+        console.log("relive")
+        //await relive(data)
+        ws.send(`relive ${data.id}`)
+    }
+    static async print(ws:any,data:any){
+        console.log("tuta")
+        client.forEach(function (value:any) {
+            if(value.readyState == 1 && value.unicid == data.id) {
+                console.log("tut")
+                value.send(data.message)
             }
         })
-    })
+    }
+    static async printAll(ws:any,data:any){
+        client.forEach(function (value:any) {
+            if(value.readyState == 1 ) {
+                return value.send(data.message)
+            }
+        })
+    }
 }
-
-
-export {controllerWS}
