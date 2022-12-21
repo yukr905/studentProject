57a12bca11a8d41c05f4205ef5f3ec6d73d0b467
yukr905 <63770062+yukr905@users.noreply.github.com>
2022-12-21 23:58:37 +0200

    handlerError

--- middleware/handlerError.ts
+++ middleware/handlerError.ts
@@ -5,5 +5,9 @@ export function handlerError (err: Error,req:Request,res:Response,next: NextFunc
     if (err instanceof ApiError) {
         return res.status(err.status).json({message: err.message})
     }
+    if (res.headersSent) {
+        return next(err);
+    }
+
     return res.status(500).json({message: "Непредвиденная ошибка!"})
 }
