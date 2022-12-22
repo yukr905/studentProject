0a42949bc6a31d019a274e2a12ac7c013f07a785
yukr905 <63770062+yukr905@users.noreply.github.com>
2022-12-22 11:12:44 +0200

    handlerError

--- middleware/handlerError.ts
+++ middleware/handlerError.ts
@@ -2,12 +2,11 @@ import {ApiError} from "./apiError"
 import { NextFunction, Request,Response } from 'express'
 
 export function handlerError (err: Error,req:Request,res:Response,next: NextFunction) {
-    if (err instanceof ApiError) {
-        return res.status(err.status).json({message: err.message})
-    }
     if (res.headersSent) {
         return next(err);
     }
-
+    if (err instanceof ApiError) {
+        return res.status(err.status).json({message: err.message})
+    }
     return res.status(500).json({message: "Непредвиденная ошибка!"})
 }
