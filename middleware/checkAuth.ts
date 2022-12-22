e762b54a58ee756aeaec60cbf2c8424ddbe682b2
yukr905 <63770062+yukr905@users.noreply.github.com>
2022-12-22 11:12:27 +0200

    checkAuth

--- middleware/checkAuth.ts
+++ middleware/checkAuth.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request,Response } from 'express'
 import jwt from "jsonwebtoken"
 import {jwtsecret} from "../config"
+import {ApiError} from "./apiError"
 
 export async function checkAuth(req:Request,res:Response,next: NextFunction){
     if (req.method === "OPTIONS") {
@@ -8,13 +9,14 @@ export async function checkAuth(req:Request,res:Response,next: NextFunction){
     }
     try {
         const token = req.headers.authorization?.split(" ")[1]
-        if (!token) {
-            return res.status(401).json({message: "Не авторизован"})
-        }
-        const decoded = await jwt.verify(token, jwtsecret)
-        req.body.token = decoded
-        next()
-    } catch (e) {
-        res.status(401).json({message: "Не авторизован"})
+    if (!token) {
+        return next(ApiError.unauth("User unauthorizen"))
+    }
+    const decoded = await jwt.verify(token, jwtsecret)
+    req.body.token = decoded
+    next()
+    } catch (error) {
+        console.log(error)
+        return next(ApiError.unauth("User unauthorizen"))
     }
 }
