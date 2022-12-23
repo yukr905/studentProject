875599d6b6d83ab347f2f6e7bbbc4ed202d58908
yukr905 <63770062+yukr905@users.noreply.github.com>
2022-12-23 16:24:48 +0200

    router

--- router/index.ts
+++ router/index.ts
@@ -3,7 +3,7 @@ import {userController}  from "../controller/userController"
 import {characterController} from"../controller/сharacterController"
 import {handlerValidation} from "../middleware/handlerValidation"
 import {loginValidation,registerValidation} from"../middleware/validation"
-import {checkAuth} from "../middleware/checkAuth"
+import {auth} from "../middleware/checkAuth"
 
 
 
@@ -11,8 +11,8 @@ const router = Router()
 
 router.post("/register",registerValidation,handlerValidation.handlerregister , userController.register)
 router.post("/login",loginValidation,handlerValidation.handlerlogin, userController.login)
-router.get("/me/:id",checkAuth ,userController.getMe)
-router.patch("/me:id",checkAuth,handlerValidation.handlerregister, userController.updateMe)
+router.get("/me/:id",auth.checkAuth ,userController.getMe)
+router.patch("/me:id",auth.checkAuth,handlerValidation.handlerregister, userController.updateMe)
 router.get("/class", characterController.getAllClass)
 
 
