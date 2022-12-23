361be858ef66e770d5e08d4ff250bba6f3ade924
yukr905 <63770062+yukr905@users.noreply.github.com>
2022-12-23 16:21:25 +0200

    userService

--- service/userService.ts
+++ service/userService.ts
@@ -1,25 +1,26 @@
 import jwt from  "jsonwebtoken"
 import {jwtsecret} from "../config"
-
-export async function registerUser(user:any){
+export class userService{
+    static async  registerUser(user:any){
    
-    // запрос к БД
-    return true 
-}
-export async function loginUser(user:any){
-    //тут должен быть запрос к БД
-    const token = jwt.sign({
-        id:user.id,email:user.email,password:user.password
-    },jwtsecret,
-    {expiresIn: '24h'})
-    return token
-}
-export async function getMeInfo(user:any){
-    //тут должен быть запрос к БД
-    return true
-}
-export async function updateUser(user:any){
-
-    //тут должен быть запрос к БД
-    return true // новый JWT 
+        // запрос к БД
+        return true 
+    }
+    static async  loginUser(user:any){
+        //тут должен быть запрос к БД
+        const token = jwt.sign({
+            id:user.id,email:user.email,password:user.password
+        },jwtsecret,
+        {expiresIn: '24h'})
+        return token
+    }
+    static async  getMeInfo(user:any){
+        //тут должен быть запрос к БД
+        return true
+    }
+    static async  updateUser(user:any){
+    
+        //тут должен быть запрос к БД
+        return true // новый JWT 
+    }
 }
