05ce808d5a0a7a38e5b8cd9b2c70742f7e9ba48a
yukr905 <63770062+yukr905@users.noreply.github.com>
2023-01-06 22:20:55 +0200

    main

--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@ import {Server}  from "socket.io"
 import {handlerError} from "./middleware/handlerError"
 import { checker } from "./middleware/handlerEvent"
 import {auth} from "./middleware/checkAuth"
+import {db} from "./models/db"
+import {Users,Classes} from "./models/models"
 
 const app= express()
 app.use(express.json())
@@ -11,13 +13,26 @@ app.use("/api" , router)
 
 app.use(handlerError)
 
-app.listen(3000,()=>{
-  console.log("Server started : 3000")
-});
+async function start() {
+  try {
+    app.listen(3000, async()=>{
+      console.log("Server started : 3000")
+    });
 
-export const io = new Server(3001)
+    await db.authenticate()
+    await Classes.sync()
+    await Users.sync()
 
+    console.log("database connection")
+  } catch (error) {
+    console.log(error)
+  }
+}
+start()
+
+const io = new Server(3001)
 io.use(auth.checkWs).on("connection", checker)
+
 
 
 
