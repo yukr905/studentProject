04a49fc1ee4d1073b6ed202c0edf103e1a2e29e5
yukr905 <63770062+yukr905@users.noreply.github.com>
2023-01-06 22:23:00 +0200

    models

---
+++ models/models.ts
@@ -0,0 +1,110 @@
+import { Model, Optional, DataTypes } from 'sequelize';
+import {db} from './db'
+
+interface classesAttributes {
+  id: number
+  name: string
+  health: number
+  damage: number
+  ability: boolean
+  attack_type: string
+}
+
+interface classesCreationAttributes
+  extends Optional<classesAttributes, 'id'> {}
+
+interface classesInstance
+  extends Model<classesAttributes, classesCreationAttributes>,
+    classesAttributes {
+      createdAt?: Date;
+      updatedAt?: Date;
+    }
+
+const Classes = db.define<classesInstance>(
+  'classes',
+  {
+    id: {
+      allowNull: false,
+      autoIncrement: false,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+      unique: true,
+    },
+    name:{
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    health:{
+      type:DataTypes.INTEGER,
+      allowNull: false,
+    },
+    damage:{
+      type:DataTypes.INTEGER,
+      allowNull: false,
+    },
+    attack_type:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    ability:{
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  }
+);
+interface usersAttributes {
+  id: number
+  username: string
+  email: string
+  password: string
+  class_id:number
+}
+
+interface usersCreationAttributes
+  extends Optional<usersAttributes, 'id'> {}
+
+interface usersInstance
+  extends Model<usersAttributes, usersCreationAttributes>,
+    usersAttributes {
+      createdAt?: Date;
+      updatedAt?: Date;
+    }
+
+const Users = db.define<usersInstance>(
+  'users',
+  {
+    id: {
+      allowNull: false,
+      autoIncrement: false,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+      unique: true,
+    },
+    username:{
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    email:{
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    password:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    class_id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  }
+);
+
+Users.belongsTo(Classes,{
+  foreignKey:"class_id",
+  as:"classes"
+})
+export {Classes,Users}
+  
