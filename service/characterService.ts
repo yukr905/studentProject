fe61c4b7a088f7e6e0c7e31c87f570f5015f54bc
yukr905 <63770062+yukr905@users.noreply.github.com>
2023-01-13 18:05:50 +0200

    characterService

--- service/characterService.ts
+++ service/characterService.ts
@@ -1,6 +1,8 @@
+import {Classes} from "../models/models"
+
 export class characterService{
-    static  async  allClass() {
-        const characters = {} // получаем с БД
+    static  async  allClass(): Promise<any> {
+        const characters = await Classes.findAll()
         return characters
     }
 }
