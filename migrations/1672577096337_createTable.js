2f25af0f020ccd259fa437997af242772fc3a933
yukr905 <63770062+yukr905@users.noreply.github.com>
2023-01-13 18:06:49 +0200

    migration

--- migrations/1672577096337_createTable.js
+++ migrations/1672577096337_createTable.js
@@ -4,7 +4,7 @@ exports.shorthands = undefined;
 
 exports.up = pgm => {
     pgm.createTable("classes",{
-        id:'id',
+        id:"id",
         name:{type:"varchar(30)",unique:true},
         health:{type:"integer",notNull: true,},
         damage:{type:"integer",notNull: true,},
@@ -15,18 +15,18 @@ exports.up = pgm => {
             notNull: true,
             default: pgm.func('current_timestamp'),
         },
-        updateAt:{
+        updatedAt:{
             type: 'timestamp',
             notNull: true,
             default: pgm.func('current_timestamp'),
         }
     })
     pgm.createTable("users", {
-        id:'id',
-        name:{type:"varchar(30)",unique:true,notNull: true,},
+        id:"id",
+        username:{type:"varchar(30)",unique:true,notNull: true,},
         email:{type:"varchar(60)",unique:true,notNull: true,},
-        password:{type:"varchar(50)",notNull: true},
-        class_Id: {
+        password:{type:"varchar(700)",notNull: true},
+        class_id: {
             type: 'integer',
             notNull: true,
             references: '"classes"',
@@ -37,14 +37,14 @@ exports.up = pgm => {
             notNull: true,
             default: pgm.func('current_timestamp'),
         },
-    updateAt:{
+    updatedAt:{
         type: 'timestamp',
         notNull: true,
         default: pgm.func('current_timestamp'),
     }
     })
-   
-    pgm.createIndex('users', 'class_Id')
+    pgm.createIndex("users","id")
+    pgm.createIndex('users', 'class_id')
 };
 
 exports.down = pgm =>{
